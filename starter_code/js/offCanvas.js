e0ab77862d2d53d153fcb21bb8b74ce896693c7e
elizabethLomb <elizabeth.lombardi7@gmail.com>
2019-12-27 15:21:17 +0100

    All done, missing toggle on click show offcanvas

---
+++ starter_code/js/offCanvas.js
@@ -0,0 +1,10 @@
+window.addEventListener("click", (event => {
+  const toggle = document.getElementById('navbar-toggler')
+  const mainNav = document.getElementById('mainNav')
+
+  if(toggle === 'collapse'){
+    return true
+  }
+
+  //event : mainNav.classList.add('show')
+}))
